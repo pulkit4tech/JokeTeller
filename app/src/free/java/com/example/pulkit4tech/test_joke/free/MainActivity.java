2771a119bcd57890d6253eb0341aba37f443f3a1
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-09-30 15:17:39 +0530

    Added Adview for free version

--- app/src/free/java/com/example/pulkit4tech/test_joke/free/MainActivity.java
+++ app/src/free/java/com/example/pulkit4tech/test_joke/free/MainActivity.java
@@ -9,18 +9,21 @@ import android.widget.TextView;
 import com.example.Jokes;
 import com.example.pulkit4tech.test_joke.MyAsyncTask;
 import com.example.pulkit4tech.test_joke.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
+    AdView adView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Jokes myjoke = new Jokes();
-        final TextView joke = (TextView) findViewById(R.id.showJoke);
-        Button button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
+        adView = (AdView) findViewById(R.id.adView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,5 +31,18 @@ public class MainActivity extends AppCompatActivity {
                 new MyAsyncTask(MainActivity.this).execute();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        adView.loadAd(adRequest);
+
     }
 }
